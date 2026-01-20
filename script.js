6d4dfd30d1a9634e095db0826b786e98e5581732
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-20 12:54:00 +0100

    Optimize code

--- script.js
+++ script.js
@@ -1,64 +1,61 @@
-console.log("Hello World!");
+console.log('Hello World!!!')
 
-let computerScore = 0;
-let humanScore = 0;
+var humanScore = 0
+var computerScore = 0
 
-function getHumanChoice() {
-    let choice = prompt("Pierre, papier, ciseaux!");
-    choice = choice.toLowerCase()
-
-    if (choice == "pierre" || choice == "papier" || choice == "ciseaux") {
-        return choice
+function getComputerChoice() {
+    const a = Math.random()
+    if (a <= 0.33) {
+        return 'rock'
+    } else if (a > 0.33 && a <= 0.66) {
+        return 'paper'   
     } else {
-        return
+        return 'scissors'
     }
 }
 
-function getComputerChoice() {
-    const choice = Math.random();
-
-    if (choice <= 0.33) {
-        return "pierre"
-    } else if (choice <= 0.66) {
-        return "papier"
-    } else if (choice <= 1) {
-        return "ciseaux"
-    } else {
-        return
-    }
+function getHumanChoice() {
+    return window.prompt('Choose between "rock", "paper" and "scissors".')
 }
 
 function playRound(humanChoice,computerChoice) {
-
-    let win = "GG! " + computerChoice + " perd contre " + humanChoice + ".";
-    let loose = "Perdu! " + computerChoice + " gagne contre " + humanChoice + ".";
-    let tie = "Ex Aequo! deux " + humanChoice + ".";
-
-    if (humanChoice == "pierre" && computerChoice == "ciseaux") {
-        humanScore ++;
-        return console.log(win);
-    } else if (humanChoice == "papier" && computerChoice == "pierre") {
-        humanScore ++;
-        return console.log(win);
-    } else if (humanChoice == "ciseaux" && computerChoice == "papier") {
-        humanScore ++;
-        return console.log(win);
-    } else if (humanChoice == computerChoice) {
-        return console.log(tie);
+    humanChoice.toLowerCase()
+    if (
+        humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper"
+        ) {
+            console.log(`You win! ${humanChoice} beat ${computerChoice}.`)
+            ++humanScore
+            return
+    } else if (humanChoice === computerChoice) {
+        console.log(`Tie! You both have chosen ${humanChoice}.`)
+        return 
     } else {
-        computerScore ++;
-        return console.log(loose);
+        console.log(`You Lose! ${computerChoice} beat ${humanChoice}.`)
+        ++computerScore
+        return 
     }
 }
 
 function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let humanSolution = getHumanChoice();
-        let computerSolution = getComputerChoice();
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log(`The game's over!`)
 
-        playRound(humanSolution,computerSolution);
+    if (humanScore > computerScore) {
+        console.log(`Congratuations! You have won with ${humanScore} points against ${computerScore}.`)
+        return
+    } else if (humanScore === computerScore) {
+        console.log(`No winner! You both have a score of ${humanScore}.`)
+        return
+    } else {
+        console.log(`Too bad! You loose with a score of ${humanScore} points against ${computerScore}.`)
+        return
     }
-    console.log("GG! Score final: VOUS = " + humanScore + " vs MACHINE = " + computerScore);
 }
 
-playGame();
+playGame()
